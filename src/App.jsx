8fe17e276019bda6aa90331c331195f6a269de8a
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2024-01-14 23:44:22 +0600

    board functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
 import { useState } from "react";
 
-function Square () {
-    const [value,setValue] = useState(null)
-
-    const handleClick = () =>{
-        setValue("X");
-    }
-
+function Square ({value,handleClick}) {
     return(
         <>
           <button onClick={handleClick} className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">{value}</button>
@@ -14,22 +8,39 @@ function Square () {
     )
 }
 const Board = () => {
+    const [value,setValue] = useState(Array(9).fill(null));
+    const [isX,setIsX] = useState(true);
+    const handleClick = (i) =>{
+        
+        if(value[i]){
+            return;
+        }
+        const nextValue = value.slice();
+        if(isX){
+            nextValue[i] = "X"
+        }
+        else{
+            nextValue[i] = "O"
+        }
+        setValue(nextValue);
+        setIsX(!isX);
+    }
     return (
         <div className="">
             <div className="flex">
-                <Square />
-                <Square />
-                <Square />
+                <Square value={value[0]} handleClick={()=>handleClick(0)} />
+                <Square  value={value[1]} handleClick={()=>handleClick(1)}/>
+                <Square  value={value[2]} handleClick={()=>handleClick(2)}/>
             </div>
             <div className="flex">
-                <Square />
-                <Square />
-                <Square />
+                <Square  value={value[3]} handleClick={()=>handleClick(3)}/>
+                <Square  value={value[4]} handleClick={()=>handleClick(4)}/>
+                <Square  value={value[5]} handleClick={()=>handleClick(5)}/>
             </div>
             <div className="flex">
-                <Square />
-                <Square />
-                <Square />
+                <Square  value={value[6]} handleClick={()=>handleClick(6)}/>
+                <Square  value={value[7]} handleClick={()=>handleClick(7)}/>
+                <Square  value={value[8]} handleClick={()=>handleClick(8)}/>
             </div>
            
         </div>
